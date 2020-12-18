7edd07e47e1a1bd50485a6aa99aff68ff3c0ee0f
sylvain amianto <samianto@gmail.com>
2020-12-18 12:23:17 +0100

    topic crud base

---
+++ src/main/java/dev/exception/repoException.java
@@ -0,0 +1,30 @@
+package dev.exception;
+
+public class repoException extends Exception {
+
+	public repoException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public repoException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public repoException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public repoException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public repoException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/main/java/dev/services/TopicService.java
+++ src/main/java/dev/services/TopicService.java
@@ -1,11 +1,12 @@
 package dev.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import dev.entity.Topic;
-import dev.entity.User;
+import dev.exception.repoException;
 import dev.repositories.topicRepository;
 
 @Service
@@ -18,11 +19,28 @@ public class TopicService {
 		this.topicRepo = topicRepo;
 	}
 
-	public List<Topic> getAll(){
+	public List<Topic> getAll() {
 		return topicRepo.findAll();
 	}
-	
 
-	
-	
+	public Topic getById(Integer id) throws repoException {
+
+		Optional<Topic> t = topicRepo.findById(id);
+		if (t.isPresent()) {
+			return t.get();
+		} else
+			throw new repoException("id inconnu");
+	}
+
+	public Topic addUpdate(Topic t) {
+		topicRepo.save(t);
+		return t;
+	}
+
+	public boolean remove(int id) throws repoException {
+		Topic t=this.getById(id);
+		topicRepo.delete(t);
+		return true;
+	}
+
 }
